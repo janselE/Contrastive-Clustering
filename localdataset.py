6bb404d4ef75859dc9c2d0624d67eb603173f72f
Jansel Herrera <janselherrera@pop-os.localdomain>
2021-10-20 22:06:11 -0500

    fixing the dataloader to load images

--- localdataset.py
+++ localdataset.py
@@ -1,18 +1,19 @@
-import torch
+from PIL import Image
 
 
 class CustomDataSet(Dataset):
     def __init__(self, main_dir, transform):
-        self.main_dir = main_dir
         self.transform = transform
-        all_imgs = os.listdir(main_dir)
-        self.total_imgs = natsort.natsorted(all_imgs)
+        self.image_list = []
+
+        for filename in glob.glob(f"{path}/*.png"):  # assuming gif
+            im = Image.open(filename)
+            self.image_list.append(im)
 
     def __len__(self):
-        return len(self.total_imgs)
+        return len(self.image_list)
 
-    def __getitem__(self, idx):
-        img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
-        image = Image.open(img_loc).convert("RGB")
+    def __getitem__(self, index):
+        image = Image.open(self.image_list[index]).convert("RGB")
         tensor_image = self.transform(image)
         return tensor_image
